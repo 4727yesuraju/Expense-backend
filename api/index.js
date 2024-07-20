c38fd6826bdb7b96916b7e9ce2d55263af3da7d8
yesuraju <4727yesuraju@gmail.com>
2024-07-20 20:39:39 +0530

    path is removed

--- api/index.js
+++ api/index.js
@@ -2,8 +2,6 @@ import express from "express";
 import dotenv from 'dotenv';
 dotenv.config();
 import cookieParser from "cookie-parser";
-import path from 'path';
-const __dirname = path.resolve();
 
 //user modules 
 import connectToDB from './config/connectToDB.js';
@@ -22,11 +20,8 @@ app.use(cookieParser());
 app.use('/api/user',userRoutes);
 app.use('/api/expense',expenseRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));  
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+
 
 app.listen(PORT,()=>{
     connectToDB();
