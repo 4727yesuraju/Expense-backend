8a65fd67201372d5a4d1e6725dd82170024d4733
yesuraju <4727yesuraju@gmail.com>
2024-07-20 20:48:42 +0530

    cors is added

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from 'dotenv';
 dotenv.config();
 import cookieParser from "cookie-parser";
-
+import cors from 'cors';
 //user modules 
 import connectToDB from './config/connectToDB.js';
 //routes
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
-//app.use(cors());
+app.use(cors());
 
 //router
 app.use('/api/user',userRoutes);
